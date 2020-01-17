cd0f0d1146d4c924c2ee81a205a833a56cdc0c47
fedodd <fedodd.s@gmail.com>
2020-01-17 15:39:27 +0300

    start project and concat google places api

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './App.css';
+import dashbord from "./components/dashbord/dashbord";
+import filter from "./components/filter/filter";
+import form from "./components/form/form";
+import {
+  ReactDadata
+} from 'react-dadata';
+import ReactDadataBox from 'react-dadata-box';
+import DadataSuggestions from 'react-dadata-suggestions';
+import "react-dadata-suggestions/dist/styles.css";
+
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+// If you want to use the provided css
+import 'react-google-places-autocomplete/dist/assets/index.css';
+
+
+
+class App extends Component {
+
+  state = {
+    autocompletionRequest: {
+      componentRestrictions: {
+        country: 'ru'
+      },
+      AutocompletePrediction: {
+        description: 'true'
+      },
+       types: ['(cities)'],
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Маябрь</h1>
+        </header>
+        <div>
+          <GooglePlacesAutocomplete
+            onSelect={event => console.log(event)}
+            placeholder="Воронеж"
+            // inputClassName="input"
+            autocompletionRequest={this.state.autocompletionRequest}
+            renderSuggestions={(active, suggestions, onSelectSuggestion) => (
+              <div className="suggestions-container">
+                {
+                  suggestions.map((suggestion) => {
+                    let descr = suggestion.description.slice(0, suggestion.description.indexOf(','));
+                    let updatedSugg = {
+                      ...suggestion,
+                      description: descr
+                    }
+                    return (
+                    <div
+                      className="suggestion"
+                        onClick={(event) => onSelectSuggestion(updatedSugg, event)}
+                    >
+                      {descr}
+                    </div>
+                  )})
+                }
+              </div>
+            )}
+          />
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
