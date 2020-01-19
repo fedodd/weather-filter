7344e95429302ca7827632bbd913f594fe35e10e
fedodd <fedodd.s@gmail.com>
2020-01-19 22:29:32 +0300

    new branch

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -5,7 +5,7 @@ import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 // If you want to use the provided css
 import 'react-google-places-autocomplete/dist/assets/index.css';
 import Button from "../button/button";
-import './form.css';
+import './form.pcss';
 
 class form extends Component {
   state = {
